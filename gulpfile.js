2f573ae6689b3739a63cca88a7e8ac62e8c81e9f
Diego Marchetti <diego.marchetti@ef5.com.br>
2020-02-17 14:23:44 -0300

    commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') 
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+  });
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    })
+  })
+
+
+gulp.task('watch', gulp.series(['browserSync']), async function(){
+    // gulp.watch('app/scss/**/*.scss', ['sass']); 
+    // Other watchers
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+})
+
+gulp.task('default', gulp.series(['watch']) , async function () {
+    
+    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch((['browserSync', 'sass', 'watch'])); 
+});
